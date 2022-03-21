dec67b9472b3ae83adc5bfd9998b9bad2040f15a
a-appelblom <a.appelblom@gmail.com>
2022-03-21 11:06:04 +0100

    should work

--- src/routes/instagram.ts
+++ src/routes/instagram.ts
@@ -2,7 +2,7 @@ const INSTAGRAM_ACCESS_TOKEN = import.meta.env.VITE_INSTAGRAM_ACCESS_TOKEN;
 const INSTAGRAM_TOKEN_EXPIRY_DATE = import.meta.env.VITE_INSTAGRAM_TOKEN_EXPIRY_DATE;
 const VERCEL_ACCESS_TOKEN = import.meta.env.VITE_VERCEL_ACCESS_TOKEN;
 const VERCEL_PROJECT_ID = import.meta.env.VITE_VERCEL_PROJECT_ID;
-const REDEPLOY_URL = import.meta.env.VITE__VERCEL_REDEPLOY_URL;
+const REDEPLOY_URL = import.meta.env.VITE_VERCEL_REDEPLOY_URL;
 const GET_TOKEN_URL = `https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${INSTAGRAM_ACCESS_TOKEN}`;
 
 export async function get() {
@@ -49,8 +49,8 @@ export async function updateInstagramToken() {
 	if (!data) return;
 	// Get env id's
 	const accessToken = data.envs.find((env) => env.key === 'VITE_INSTAGRAM_ACCESS_TOKEN');
-	const expiry = data.envs.find((env) => env.key === 'VITE_INSTAGRAM_EXPIRY_DATE');
-	console.log(accessToken, expiry);
+	const expiry = data.envs.find((env) => env.key === 'VITE_INSTAGRAM_TOKEN_EXPIRY_DATE');
+	console.log(accessToken, expiry.id);
 
 	// Update the ENV
 	if (accessToken && expiry) {
@@ -72,14 +72,14 @@ export async function updateInstagramToken() {
 			},
 			body: JSON.stringify({
 				// value: Date.now() + newToken.expires_in * 1000 - 1000 * 60 * 60 * 24 * 7
-				value: Date.now() + 1000 * 60 * 10
+				value: `${Date.now() + 1000 * 60 * 10}`
 			})
 		});
-		console.log('___________________________________________________________');
+		// console.log('___________________________________________________________');
 		console.log(accessUpdateResponse, expiryUpdateResponse);
 
 		// Trigger redeploy
-		if (accessToken.status === 200 && expiry.status === 200) {
+		if (accessUpdateResponse.status === 200 && expiryUpdateResponse.status === 200) {
 			console.log('___________________________________________________________');
 			console.log('Token updated');
 			await fetch(REDEPLOY_URL as string);
